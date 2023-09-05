fb5719024545aacf8ab1b9dc1f2dff0bebe55306
OptimumCOW <kenallan18@hotmail.com>
2023-09-05 11:44:23 +0200

    removed covid stats and made timers work but not persist.

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-async function fetchData() {
-  const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data");
-  const record = await res.json();
-  document.getElementById("date").innerHTML = record.data[0].date;
-  document.getElementById("areaName").innerHTML = record.data[0].areaName;
-  document.getElementById("latestBy").innerHTML = record.data[0].latestBy;
-  document.getElementById("deathNew").innerHTML = record.data[0].deathNew;
-}
+const slightlyStressedPeriod = 10 * 1000;
+const reallyStressedPeriod = slightlyStressedPeriod + 5 * 1000;
+const timerIncrement = 10;
 
-fetchData();
+let timer = 0;
+let chimesRang = 0;
+let interval;
+
+const renderTimer = () => {
+  document.getElementById("timer").innerHTML = timer;
+};
+
+const stopTimer = () => {
+  clearInterval(interval);
+
+  timer = 0;
+  chimesRang = 0;
+  interval = null;
+
+  renderTimer();
+};
+
+const incrementTimer = () => {
+  if (timer == reallyStressedPeriod) {
+    stopTimer();
+    return;
+  }
+
+  timer += timerIncrement;
+
+  renderTimer();
+};
+
+const playSound = () => {
+  const minuteCount = timer / (1 * 1000);
+  if (chimesRang >= minuteCount) return;
+
+  // play sound => depending on where in slightly stressed it is.
+  // increment chimes Rang
+};
+
+const handleIncrement = () => {
+  incrementTimer();
+  playSound();
+};
+
+const startTimer = () => {
+  document.getElementById("state").innerHTML =
+    timer !== 0 ? "timer running" : "timer stopped";
+
+  interval = setInterval(handleIncrement, timerIncrement);
+};
+
+document.getElementById("startButton").addEventListener("click", startTimer);
+document.getElementById("stopButton").addEventListener("click", stopTimer);
